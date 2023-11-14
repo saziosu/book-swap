27bc758dcdcc686a568ba180d1fb26eb05aaf980
saziosu <>
2023-11-14 21:17:13 +0000

    Wired up BookDeleteView in urls.py

--- tradeapp/urls.py
+++ tradeapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<slug:slug>/', views.BookDetail.as_view(), name='book_detail'),
     path('add-book', views.BookCreateView.as_view(), name='book_create'),
     path('<slug:slug>/edit', views.BookUpdateView.as_view(), name='book_update'),
+    path('<slug:slug>/delete', views.BookDeleteView.as_view(), name='book_delete'),
 ]
