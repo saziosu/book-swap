b80f34323d8bcb3de5286ee85ea6f0f72fd075f7
saziosu <>
2023-11-22 17:36:31 +0000

    Updated urls.py to improve long lines for linting

--- tradeapp/urls.py
+++ tradeapp/urls.py
@@ -3,9 +3,22 @@ from django.urls import path
 from django.http import HttpResponse
 
 urlpatterns = [
+    # url for the root page
     path('', views.BookView.as_view(), name='home'),
-    path('<slug:slug>/', views.BookDetail.as_view(), name='book_detail'),
-    path('add-book', views.BookCreateView.as_view(), name='book_create'),
-    path('<slug:slug>/edit', views.BookUpdateView.as_view(), name='book_update'),
-    path('<slug:slug>/delete', views.BookDeleteView.as_view(), name='book_delete'),
+    # url for the book detail page
+    path('<slug:slug>/',
+         views.BookDetail.as_view(),
+         name='book_detail'),
+    # url for adding books
+    path('add-book',
+         views.BookCreateView.as_view(),
+         name='book_create'),
+    # url for editing the book post
+    path('<slug:slug>/edit',
+         views.BookUpdateView.as_view(),
+         name='book_update'),
+    # url for deleting the book post
+    path('<slug:slug>/delete',
+         views.BookDeleteView.as_view(),
+         name='book_delete'),
 ]
